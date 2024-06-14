b908e8e511145e9480344e8cc724630578dd8478
horatiu84 <horatiul@yahoo.com>
2024-06-14 11:52:33 +0300

    starting the split bill logic

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -1,59 +1,83 @@
 import Friend from "./Friend";
 import AddFriendForm from "./AddFriendForm";
 import SplitABillForm from "./SplitABillForm";
-import {useState} from "react";
-import './FriendList.css'
+import { useState } from "react";
+import "./FriendList.css";
 
 const friends = [
-    { name: 'Clark',
-      photo : 'https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
-      status: 'You owe Clark 7$'
-    },
-    { name: 'Sarah',
-      photo : 'https://media.meer.com/attachments/fab1026dcd503986811212d6bf4f44b28551991c/store/fill/860/645/95695d4e4c4394fb92cc829a02c7b4ed57dae7e1d0e9a5c2cf9600084c5e/A-bright-woman.jpg',
-      status: 'Sarah owes you 20$'
-    },
-    { name: 'Anthony',
-      photo : 'https://img.freepik.com/free-photo/handsome-bearded-guy-posing-against-white-wall_273609-20597.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1718150400&semt=sph',
-      status: 'You and Anthony are even'
-    },
+  {
+    name: "Clark",
+    photo:
+      "https://images.pexels.com/photos/2379005/pexels-photo-2379005.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    status: "You owe Clark 7$",
+  },
+  {
+    name: "Sarah",
+    photo:
+      "https://media.meer.com/attachments/fab1026dcd503986811212d6bf4f44b28551991c/store/fill/860/645/95695d4e4c4394fb92cc829a02c7b4ed57dae7e1d0e9a5c2cf9600084c5e/A-bright-woman.jpg",
+    status: "Sarah owes you 20$",
+  },
+  {
+    name: "Anthony",
+    photo:
+      "https://img.freepik.com/free-photo/handsome-bearded-guy-posing-against-white-wall_273609-20597.jpg?size=626&ext=jpg&ga=GA1.1.1141335507.1718150400&semt=sph",
+    status: "You and Anthony are even",
+  },
 ];
 
 function FriendList() {
-  const [addToggle,setAddToggle] = useState(false);
+  const [addToggle, setAddToggle] = useState(false);
   const [curSelected, setCurSelected] = useState(null);
-  const [data,setData] = useState([...friends])
+  const [data, setData] = useState([...friends]);
 
   function handleAddFriend(newFriend) {
-    setData([...data,newFriend])
+    setData([...data, newFriend]);
+  }
+
+  function handleUpdateFriend(friend) {
+    const newData = [...data];
+    newData.splice(curSelected, 1, friend);
+    setData(newData);
   }
 
   function handleToggleAdd() {
-    setAddToggle( cur => !cur)
+    setAddToggle((cur) => !cur);
   }
-    return (
-      <div className="app">
-      
-        <div>
-            {data.map( (friend,i) => {
-                return  <Friend 
-                          name={friend.name} 
-                          onCurSelect={setCurSelected} 
-                          curSelected={curSelected} 
-                          status={friend.status} 
-                          photo={friend.photo} 
-                          num={i} 
-                          key={i} />
-            })}
-            
-            { !addToggle && <button onClick={handleToggleAdd}>Add friend</button>}
-            { addToggle && <AddFriendForm onToggleAdd={handleToggleAdd} handleAddFriend={handleAddFriend}/>}
-        </div>
-        <div>
-          {curSelected !== null && <SplitABillForm name={friends[curSelected].name} />}
-        </div>
+  return (
+    <div className="app">
+      <div>
+        {data.map((friend, i) => {
+          return (
+            <Friend
+              name={friend.name}
+              onCurSelect={setCurSelected}
+              curSelected={curSelected}
+              status={friend.status}
+              photo={friend.photo}
+              num={i}
+              key={i}
+            />
+          );
+        })}
+
+        {!addToggle && <button onClick={handleToggleAdd}>Add friend</button>}
+        {addToggle && (
+          <AddFriendForm
+            onToggleAdd={handleToggleAdd}
+            handleAddFriend={handleAddFriend}
+          />
+        )}
       </div>
-    )
+      <div>
+        {curSelected !== null && (
+          <SplitABillForm
+            friend={data[curSelected]}
+            onUpdateFriend={handleUpdateFriend}
+          />
+        )}
+      </div>
+    </div>
+  );
 }
 
 export default FriendList;
--- src/components/SplitABillForm.js
+++ src/components/SplitABillForm.js
@@ -1,32 +1,37 @@
-import './SplitABillForm.css';
-function SplitABillForm({name}) {
-    return (
-        <div className="splitForm">
-        <h3>SPLIT A BILL WITH {name}</h3>
-        <div className="splitForm">
-            <div>
-                <label>Bill value</label>
-                <input type="text" name="bill"/>
-            </div>
-            <div>
-                <label>Your expense</label>
-                <input type="text" name="yourExpense"/>
-            </div>
-            <div>
-                <label>{name}'s expense</label>
-                <input type="text" name="friendExpense"/>
-            </div>
-            <div>
-            <label>Who is paying the bill?</label>
-            <select name="payBill">
-                <option value='you'>You</option>
-                <option value='friend'>{name}</option>
-            </select>
-            </div>
-            <button>Split bill</button>
+import "./SplitABillForm.css";
+function SplitABillForm({ friend, onUpdateFriend }) {
+  const description = "test";
+  function handleBillSplit() {
+    const updatedFriend = { ...friend, status: description };
+    onUpdateFriend(updatedFriend);
+  }
+  return (
+    <div className="splitForm">
+      <h3>SPLIT A BILL WITH {friend.name}</h3>
+      <div className="splitForm">
+        <div>
+          <label>Bill value</label>
+          <input type="text" name="bill" />
         </div>
+        <div>
+          <label>Your expense</label>
+          <input type="text" name="yourExpense" />
         </div>
-    )
+        <div>
+          <label>{friend.name}'s expense</label>
+          <input type="text" name="friendExpense" />
+        </div>
+        <div>
+          <label>Who is paying the bill?</label>
+          <select name="payBill">
+            <option value="you">You</option>
+            <option value="friend">{friend.name}</option>
+          </select>
+        </div>
+        <button onClick={handleBillSplit}>Split bill</button>
+      </div>
+    </div>
+  );
 }
 
 export default SplitABillForm;
